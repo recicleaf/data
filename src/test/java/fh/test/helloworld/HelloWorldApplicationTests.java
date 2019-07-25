07cee59429a2950661abdd90461b0a41bf7ec651
Felipe Hernandez <fllamas@travix.com>
2019-07-25 16:40:54 +0100

    fix POC postgres integration

--- src/test/java/fh/test/helloworld/HelloWorldApplicationTests.java
+++ src/test/java/fh/test/helloworld/HelloWorldApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("development")
+@ActiveProfiles("staging")
 public class HelloWorldApplicationTests {
 
     @Test
